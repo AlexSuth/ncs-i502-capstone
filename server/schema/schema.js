adaf1ed10f248bc259dfb8610fdf8efc40bfffa1
AlexSuth <AlexanderNSutherland@gmail.com>
2021-05-16 19:48:50 -0700

    creates products schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,13 +7,41 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
+  GraphQLList,
 } = graphql;
 
 // dummy data
 var categories = [
   { name: 'Flower', photoUrl: 'www.fakeurl.fake', id: '1' },
   { name: 'Extracts', photoUrl: 'www.fakeurl.fake', id: '2' },
-  { name: 'Vaporizers', photoUrl: 'www.fakeurl.fake', id: '3' },
+  { name: 'Edibles', photoUrl: 'www.fakeurl.fake', id: '3' },
+];
+
+var products = [
+  {
+    name: 'Northern Lights 3.5g',
+    photoUrl: 'www.fakeurl.fake',
+    id: '1',
+    thc: '19.4%',
+    cbd: '0.2%',
+    categoryId: '1',
+  },
+  {
+    name: 'Yummy Chews 1:1',
+    photoUrl: 'www.fakeurl.fake',
+    id: '2',
+    thc: '100mg',
+    cbd: '100mg',
+    categoryId: '3',
+  },
+  {
+    name: 'White Widow Sauce 1g',
+    photoUrl: 'www.fakeurl.fake',
+    id: '3',
+    thc: '78.4%',
+    cbd: '2.2%',
+    categoryId: '2',
+  },
 ];
 
 const CategoryType = new GraphQLObjectType({
@@ -22,6 +50,24 @@ const CategoryType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     photoUrl: { type: GraphQLString },
+  }),
+});
+
+const ProductType = new GraphQLObjectType({
+  name: 'Product',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    photoUrl: { type: GraphQLString },
+    thc: { type: GraphQLString },
+    cbd: { type: GraphQLString },
+    category: {
+      type: new GraphQLList(CategoryType),
+      resolve(parent, args) {
+        console.log(parent);
+        return _.filter(categories, { id: parent.categoryId });
+      },
+    },
   }),
 });
 
@@ -35,6 +81,12 @@ const RootQuery = new GraphQLObjectType({
         //code to get data from db
         console.log(typeof args.id);
         return _.find(categories, { id: args.id });
+      },
+    },
+    products: {
+      type: new GraphQLList(ProductType),
+      resolve(parent, args) {
+        return products;
       },
     },
   },
