538cded7ceb308f176a32c024545cce5a3268942
AllisonBC <allycm07@gmail.com>
2021-05-22 22:20:27 -0700

    Starter createAccount page
    To do:
    pull in cart page changes
    fix header overlap
    add 'company' details to form

--- i502portal/src/pages/CreateAccount/createAccount.component.jsx
+++ i502portal/src/pages/CreateAccount/createAccount.component.jsx
@@ -1,10 +1,106 @@
 import React from 'react';
 
-const CreateAccountPage = () => (
-  <div className = 'create-account'>
-    Create Account
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-  </div>
-)
+import useStyles from './createAccount.styles';
+import logo from '../../assets/e-commerce.png';
+
+
+const CreateAccountPage = () => {
+  const classes = useStyles();
+
+return (
+  <Container component="main" maxWidth="xs">
+    <CssBaseline />
+    <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+      <img
+          src={logo}
+          alt='i502 Portal'
+          height='25px'
+        />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Register as a retailer
+      </Typography>
+      <form className={classes.form} noValidate>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              autoComplete="fname"
+              name="firstName"
+              variant="outlined"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              autoFocus
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoComplete="lname"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              variant="outlined"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          className={classes.submit}
+        >
+          Sign Up
+        </Button>
+        <Grid container justify="flex-end">
+          <Grid item>
+            <Link href="/login" variant="body2">
+              Already have an account? Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </div>
+  </Container>
+);
+}
 
 export default CreateAccountPage;
