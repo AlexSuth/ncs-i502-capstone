89ebe0a0c59be776bf59d28c0a3ea710680457d7
AllisonBC <allycm07@gmail.com>
2021-05-23 21:26:41 -0700

    Update form fields

--- i502portal/src/pages/CreateAccount/createAccount.component.jsx
+++ i502portal/src/pages/CreateAccount/createAccount.component.jsx
@@ -16,230 +16,190 @@ import logo from '../../assets/e-commerce.png';
 const CreateAccountPage = () => {
   const classes = useStyles();
 
-return (
-  <Container component="main" maxWidth="xs">
-    <CssBaseline />
-    <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-      <img
-          src={logo}
-          alt='i502 Portal'
-          height='25px'
-        />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Register as a Retailer
-      </Typography>
-
-      <form className={classes.form} noValidate>
-        <Grid container spacing={2}>
-        <Typography component="h1" variant="h6">
-        User Information
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+        <img
+            src={logo}
+            alt='i502 Portal'
+            height='25px'
+          />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Register as a Retailer
         </Typography>
-          <Grid item xs={12}>
-            <TextField
-              autoComplete="fname"
-              name="firstName"
-              variant="outlined"
-              required
-              fullWidth
-              id="firstName"
-              label="First Name"
-              autoFocus
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="lastName"
-              label="Last Name"
-              name="lastName"
-              autoComplete="lname"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-            />
-          </Grid>
+
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
           <Typography component="h1" variant="h6">
-        Company Information
-      </Typography>
-      <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="company-name"
-              label="Company Name"
-              id="company-name"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="position"
-              label="Position"
-              id="position"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="company-street-address"
-              label="Company Street Address"
-              type="address"
-              id="company-street-address"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="city"
-              label="City"
-              type="city"
-              id="company-city"
-            />            
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="state"
-              label="State/Province"
-              id="company-state"
-            />            
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="zip"
-              label="Zip Code/Postal Code"
-              id="company-zip"
-            />            
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="phone"
-              label="Phone"
-              id="company-phone"
-            />            
-          </Grid>
-          <Typography component="h1" variant="h6">
-            License Information
+          User Information
           </Typography>
-      <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="license-number"
-              label="License Number"
-              id="license-number"
-            />
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+              />
+            </Grid>
+            <Typography component="h1" variant="h6">
+          Company Information
+        </Typography>
+        <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="company-name"
+                label="Company Name"
+                id="company-name"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="company-street-address"
+                label="Street Address"
+                id="company-street-address"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="city"
+                label="City"
+                type="city"
+                id="company-city"
+              />            
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="state"
+                label="State/Province"
+                id="company-state"
+              />            
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="zip"
+                label="Zip Code/Postal Code"
+                id="company-zip"
+              />            
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="phone"
+                label="Phone"
+                id="company-phone"
+              />            
+            </Grid>
+            <Typography component="h1" variant="h6">
+              License Information
+            </Typography>
+        <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="license-number"
+                label="License Number"
+                id="license-number"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="expiration-date"
+                label="Expiration Date"
+                id="expiration-date"
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="license-type"
-              label="License Type"
-              id="license-type"
-            />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              fullWidth
-              name="issue-date"
-              label="Issue Date"
-              id="issue-date"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="expiration-date"
-              label="Expiration Date"
-              id="expiration-date"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              variant="outlined"
-              required
-              fullWidth
-              name="license-document"
-              type="file"
-              id="license-document"
-            />            
-          </Grid>
-        </Grid>
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-        >
-          Sign Up
-        </Button>
-        <Grid container justify="flex-end">
-          <Grid item>
-            <Link href="/login" variant="body2">
-              Already have an account? Sign in
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-    </div>
-  </Container>
-);
+        </form>
+      </div>
+    </Container>
+  );
 }
 
 export default CreateAccountPage;
