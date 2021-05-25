2f86c71df6e900432b0bbd7d2cad4e89517b4bda
AllisonBC <allycm07@gmail.com>
2021-05-25 11:17:27 -0700

    Formatting and images

--- i502portal/src/components/Views/ShopSections/shopSections.component.jsx
+++ i502portal/src/components/Views/ShopSections/shopSections.component.jsx
@@ -1,25 +1,100 @@
 import React from 'react';
 
-import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+import FlowerImage from '../../../assets/flower.png';
+import VialImage from '../../../assets/vial.png';
+import CookieImage from '../../../assets/cookies.png';
+
 
 import useStyles from './shopSections.styles';
-import logo from '../../../assets/e-commerce.png';
 
 const ShopSections = () => {
   const classes = useStyles();
 
-
   return (
-    <div className={classes.paper}>
-      <Avatar className={classes.avatar}>
-        <img
-          src={logo}
-          alt='i502 Portal'
-          height='25px'
-        />
-      </Avatar>
-    </div>
-  );
-};
+
+    <Container className={classes.container} component="section" maxWidth="xl" alignItems= 'center'>
+        <Grid container spacing={8}>
+              <Grid item key='1'>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={FlowerImage}
+                    title="Flower Image"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Flowers
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  <Link href="/products/">
+                    <Button variant="contained" size="medium" color="primary">
+                      Shop Collection
+                    </Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+                </Grid>
+
+                <Grid item key='2'>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={VialImage}
+                    title="Extracts Image"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Extracts
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  <Link href="/products/">
+                    <Button variant="contained" size="medium" color="primary">
+                      Shop Collection
+                    </Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+                </Grid>
+
+                <Grid item key='3'>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={CookieImage}
+                    title="Edibles Image"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                    Edibles
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                  <Link href="/products/">
+                    <Button variant="contained" size="medium" color="primary">
+                      Shop Collection
+                    </Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+              </Grid>
+              </Grid>
+        </Container> 
+            );
+  }
+
+
+
 
 export default ShopSections;
