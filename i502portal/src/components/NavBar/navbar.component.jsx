8de300a03a195a50528ab7c3fe4cdf29a4c76000
Andy <38418756+andylwilson@users.noreply.github.com>
2021-05-24 17:19:38 -0700

    initial version
    working hamburger button

--- i502portal/src/components/NavBar/navbar.component.jsx
+++ i502portal/src/components/NavBar/navbar.component.jsx
@@ -14,15 +14,11 @@ import {
   Container,
 } from '@material-ui/core';
 
-import { Home } from '@material-ui/icons'
-
-import { ShoppingCart } from '@material-ui/icons';
-
+import { Home, ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
-
 import useStyles from './navbar.styles';
 
-import logo from '../../assets/e-commerce.png';
+import SideDrawer from './sidedrawer.component';
 
 const navLinks = [
   { title: 'Home', path: '/home' },
@@ -30,29 +26,13 @@ const navLinks = [
   { title: 'Products', path: 'products' },
   { title: 'Login', path: '/login' },
   // { title: 'Create Account', path: '/create-account' },
-  // { title: 'Edit Products', path: '/edit-products' },
   // { title: 'Reset Password', path: '/reset' },
 ]
-
-// const useStyle = makeStyles({
-//   navDisplayFlex: {
-//     display: 'flex',
-//     justifyContent: 'space-between',
-//     backgroundColor: 'lightblue'
-//   },
-//   linkText: {
-//     textDecoration: 'none',
-//     textTransform: 'uppercase',
-//     color: 'black'
-//   }
-// });
 
 const NavBar = ({ totalItems }) => {
   const classes = useStyles();
 
   const location = useLocation();
-
-  // const styles = useStyle();
 
   return (
     <>
@@ -65,37 +45,38 @@ const NavBar = ({ totalItems }) => {
           
         <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
 
-        <IconButton edge="start" color="inherit" aria-label="home">
-          <Home fontSize="large" />
-        </IconButton>
-        
-    {navLinks.map(({ title, path }) => (
-      <a href={path} key={title} className={classes.linkText}>
-        <ListItem button>
-          <ListItemText primary={title} />
-        </ListItem>
-      </a>
-    ))}
-    <IconButton aria-label='Show Cart Items' color='inherit'>
-              <Badge badgeContent={totalItems} color='secondary'>
-                <ShoppingCart />
-              </Badge>
-            </IconButton>
-    {location.pathname !== '/cart' && (
-                <div className={classes.button}>
-                  <IconButton
-                    component={Link}
-                    to='/cart'
-                    aria-label='Show Cart Items'
-                    color='inherit'
-                  >
-                    <Badge badgeContent={totalItems} color='secondary'>
-                      <ShoppingCart />
-                    </Badge>
-                  </IconButton>
-                </div>
-              )}
-  </List>
+          <IconButton edge="start" color="inherit" aria-label="home">
+            <Home fontSize="large" />
+          </IconButton>
+          {navLinks.map(({ title, path }) => (
+            <a href={path} key={title} className={classes.linkText}>
+          <ListItem button>
+            <ListItemText primary={title} />
+          </ListItem>
+        </a>
+      ))}
+      <IconButton aria-label='Show Cart Items' color='inherit'>
+                <Badge badgeContent={totalItems} color='secondary'>
+                  <ShoppingCart />
+                </Badge>
+              </IconButton>
+      {location.pathname !== '/cart' && (
+                  <div className={classes.button}>
+                    <IconButton
+                      component={Link}
+                      to='/cart'
+                      aria-label='Show Cart Items'
+                      color='inherit'
+                    >
+                      <Badge badgeContent={totalItems} color='secondary'>
+                        <ShoppingCart />
+                      </Badge>
+                    </IconButton>
+                  </div>
+                )}
+    </List>
+    
+  <SideDrawer navLinks={navLinks} />
   
   </Container>
             
